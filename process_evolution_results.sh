f214662192afad87c4fafc7791bb5e136bf055e4
mhnnunes@pandora <mhnnunes@dcc.ufmg.br>
2019-10-24 18:08:50 -0300

    Update results analysis

---
+++ process_evolution_results.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DATABASE=$1
+DIR=$2
+
+cat ${DIR}/results_evolution_${DATABASE} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
+
+cat ${DIR}/results_evolution_${DATABASE} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
+
+paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATABASE}
+
+rm ${DIR}/parent_temp ${DIR}/child_temp
+
+cat ${DIR}/results_evolution_${DATABASE} | grep -E '(time|Time)' > ${DIR}/time_${DATABASE}
+
+START_INITIAL=`grep -n initial\ random\ population\ =  results/seed_123/results_evolution_citeseer | cut -d':' -f 1`
+END_INITIAL=`grep -n initial\ random\ population\ D  results/seed_123/results_evolution_citeseer | cut -d':' -f 1`
+DIFF=`echo  $END_INITIAL - $START_INITIAL  + 1 | bc`
+head -n ${END_INITIAL} ${DIR}/results_evolution_${DATABASE} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATABASE}
+
