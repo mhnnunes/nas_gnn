ec212329ad3239c02e25351206fd9cfc283c2d0a
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2019-12-03 22:13:33 -0300

    Add modularization to trainer, evolution trainer working

---
+++ graphnas/evolution_trainer.py
@@ -0,0 +1,147 @@
+import time
+import torch
+import numpy as np
+from collections import deque
+from graphnas.trainer import Trainer
+
+
+class Evolution_Trainer(Trainer):
+    """
+    This class implements the Asyncronous Aging Evolution,
+    proposed by Real et. al. on:
+
+    Regularized Evolution for Image Classifier Architecture Search
+
+    available on: https://arxiv.org/abs/1802.01548
+    """
+    def __init__(self, args):
+        super(Evolution_Trainer, self).__init__(args)
+        self.args = args
+        self.random_seed = args.random_seed
+        self.population = deque()
+        self.accuracies = deque()
+        self.population_size = args.population_size
+        self.sample_size = args.sample_size
+        self.cycles = args.cycles
+        self.init_time = 0
+        print('initializing population on evolution_trainer init, maybe not the best strategy')
+        self.__initialize_population()
+
+    def _construct_action(self, actions):
+        structure_list = []
+        for single_action in actions:
+            structure = []
+            print('single_action: ', single_action)
+            for action, action_name in zip(single_action, self.action_list):
+                predicted_actions = self.search_space[action_name][action]
+                structure.append(predicted_actions)
+            structure_list.append(structure)
+        return structure_list
+
+    def _generate_random_individual(self):
+        ind = []
+        for action in self.action_list:
+            ind.append(np.random.randint(0,
+                                         len(self.search_space[action])))
+        return ind
+
+    def derive_from_population(self):
+        population = self._construct_action(self.population)
+        best_score_index, _ = \
+            self._get_best_individual_accuracy(self.accuracies)
+        best_structure = self.form_gnn_info(population[best_score_index])
+        print("[DERIVE] Best Structure:", str(best_structure))
+        # train from scratch to get the final score
+        np.random.seed(self.random_seed)
+        torch.manual_seed(self.random_seed)
+        torch.cuda.manual_seed_all(self.random_seed)
+        test_scores_list = []
+        for i in range(10):  # run 10 times to get Mean and Stddev
+            val_acc, test_acc = self.submodel_manager.evaluate(best_structure)
+            test_scores_list.append(test_acc)
+        print("[DERIVE] Best Results: ", best_structure, ": ",
+              np.mean(test_scores_list),
+              "+/-", np.std(test_scores_list))
+
+    def _mutate_individual(self, indiv):
+        # Choose a random position on the individual to mutate
+        position_to_mutate = np.random.randint(len(indiv))
+        # This position will receive a randomly chosen index
+        # of the search_spaces's list
+        # for the action corresponding to that position in the individual
+        sp_list = self.search_space[self.action_list[position_to_mutate]]
+        indiv[position_to_mutate] = \
+            np.random.randint(0, len(sp_list))
+        return indiv
+
+    def _get_best_individual_accuracy(self, accs):
+        max_acc_index = 0
+        max_acc = -1
+        for index, acc in enumerate(accs):
+            if acc > max_acc:
+                max_acc = acc
+                max_acc_index = index
+        return max_acc_index, max_acc
+
+    def __initialize_population(self):
+        print("\n\n===== Evaluating initial random population =====")
+        start_initial_population_time = time.time()
+        while len(self.population) < self.population_size:
+            # print('adding individual #:', len(population))
+            individual = self._generate_random_individual()
+            ind_actions = self._construct_action([individual])
+            gnn = self.form_gnn_info(ind_actions[0])
+            _, ind_acc = \
+                self.submodel_manager.train(gnn, format=self.args.format)
+            print("individual:", individual, " val_score:", ind_acc)
+            self.accuracies.append(ind_acc)
+            self.population.append(individual)
+        end_initial_pop_time = time.time()
+        self.init_time = end_initial_pop_time - start_initial_population_time
+        print("Time elapsed initializing population: " +
+              str(self.init_time))
+        print("===== Evaluating initial random population DONE ====")
+
+    def train(self):
+        print("\n\n===== Evolution ====")
+        start_evolution_time = time.time()
+        while self.cycles > 0:
+            sample = []  # list with indexes to population individuals
+            sample_accs = []  # accuracies of the sampled individuals
+            while len(sample) < self.sample_size:
+                candidate = np.random.randint(0, len(self.population))
+                sample.append(self.population[candidate])
+                sample_accs.append(self.accuracies[candidate])
+
+            # Get best individual on sample to serve as parent
+            max_sample_acc_index, max_sample_acc = \
+                self._get_best_individual_accuracy(sample_accs)
+            parent = sample[max_sample_acc_index]
+            # print('parent: ', parent)
+            child = self._mutate_individual(parent)
+            # print('child: ', child)
+            child_actions = self._construct_action([child])
+            gnn = self.form_gnn_info(child_actions[0])
+            _, child_acc = \
+                self.submodel_manager.train(gnn, format=self.args.format)
+            # print('child acc: ', child_acc)
+            print("parent: ", str(parent), " val_score: ", str(max_sample_acc),
+                  "| child: ", str(child), ", val_score: ", str(child_acc))
+            self.accuracies.append(child_acc)
+            self.population.append(child)
+            if self.cycles % self.args.eval_cycle == 0:
+                self.derive_from_population()
+            # Remove oldest individual (Aging/Regularized evolution)
+            self.population.popleft()
+            self.accuracies.popleft()
+            self.cycles -= 1
+        end_evolution_time = time.time()
+        total_evolution_time = end_evolution_time - start_evolution_time
+        print('Time spend on evolution: ' +
+              str(total_evolution_time))
+        print('Total elapsed time: ' +
+              str(total_evolution_time + self.init_time))
+        print("===== Evolution DONE ====")
+
+    def derive(self, sample_num=None):
+        self.derive_from_population()
--- graphnas/main.py
+++ graphnas/main.py
@@ -6,15 +6,8 @@ import torch
 import argparse
 import numpy as np
 from sys import exit
-from collections import deque
 import graphnas.utils.tensor_utils as utils
-from graphnas.gnn_model_manager import CitationGNNManager
-from graphnas_variants.macro_graphnas.pyg.pyg_gnn_model_manager import GeoCitationManager
-from graphnas_variants.micro_graphnas.micro_search_space import IncrementSearchSpace
-from graphnas_variants.micro_graphnas.micro_model_manager import MicroCitationManager
-# import removed from the middle of the code
-from graphnas.search_space import MacroSearchSpace
-from graphnas.graphnas_controller import SimpleNASController
+from graphnas.evolution_trainer import Evolution_Trainer
 
 
 def build_args():
@@ -80,76 +73,6 @@ def register_default_args(parser):
                         default=f"sub_manager_logger_file_{time.time()}.txt")
 
 
-def _construct_action(actions, action_list, search_space):
-    structure_list = []
-    for single_action in actions:
-        structure = []
-        # print('single_action: ', single_action)
-        for action, action_name in zip(single_action, action_list):
-            predicted_actions = search_space[action_name][action]
-            structure.append(predicted_actions)
-        structure_list.append(structure)
-    return structure_list
-
-
-def generate_random_individual(search_space, action_list):
-    ind = []
-    for action in action_list:
-        ind.append(np.random.randint(0, len(search_space[action])))
-    return ind
-
-
-def mutate_individual(indiv, search_space, action_list):
-    # Choose a random position on the individual to mutate
-    position_to_mutate = np.random.randint(len(indiv))
-    # This position will receive a randomly chosen index
-    # of the search_spaces's list
-    # for the action corresponding to that position in the individual
-    indiv[position_to_mutate] = \
-        np.random.randint(0,
-                          len(search_space[action_list[position_to_mutate]]))
-    return indiv
-
-
-def get_best_individual_accuracy(accs):
-    max_acc_index = 0
-    max_acc = -1
-    for index, acc in enumerate(accs):
-        if acc > max_acc:
-            max_acc = acc
-            max_acc_index = index
-    return max_acc_index, max_acc
-
-
-def derive_from_population(random_seed, population, accs, submodel_manager, args):
-    best_score_index, best_score = get_best_individual_accuracy(accs)
-    best_structure = form_gnn_info(population[best_score_index], args)
-    print("[DERIVE] Best Structure:" + str(best_structure))
-    # train from scratch to get the final score
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    test_scores_list = []
-    for i in range(10):  # run 100 times to get Mean and Stddev
-        # manager.shuffle_data()
-        val_acc, test_acc = submodel_manager.evaluate(best_structure)
-        test_scores_list.append(test_acc)
-    print(f"[DERIVE] Best Results: {best_structure}: {np.mean(test_scores_list):.8f} +/- {np.std(test_scores_list)}")
-
-
-def form_gnn_info(gnn, args):
-    if args.search_mode == "micro":
-        actual_action = {}
-        if args.predict_hyper:
-            actual_action["action"] = gnn[:-4]
-            actual_action["hyper_param"] = gnn[-4:]
-        else:
-            actual_action["action"] = gnn
-            actual_action["hyper_param"] = [0.005, 0.8, 5e-5, 128]
-        return actual_action
-    return gnn
-
-
 def main(args):  # pylint:disable=redefined-outer-name
 
     if args.cuda and not torch.cuda.is_available():  # cuda is not available
@@ -157,117 +80,20 @@ def main(args):  # pylint:disable=redefined-outer-name
 
     torch.manual_seed(args.random_seed)
     np.random.seed(args.random_seed)
-
+    np.set_printoptions(precision=8)
     if args.cuda:
         torch.cuda.manual_seed(args.random_seed)
 
     utils.makedirs(args.dataset)
 
-    if args.search_mode == 'macro':
-        search_space_cls = MacroSearchSpace()
-        search_space = search_space_cls.get_search_space()
-        action_list = \
-            search_space_cls.generate_action_list(args.layers_of_child_model)
-        if args.dataset in ["cora", "citeseer", "pubmed"]:
-            # implements based on dgl
-            submodel_manager = CitationGNNManager(args)
-        if args.dataset in ["Cora", "Citeseer", "Pubmed"]:
-            # implements based on pyg
-            submodel_manager = GeoCitationManager(args)
-    elif args.search_mode == 'micro':
-        args.format = "micro"
-        args.predict_hyper = True
-        args.num_of_cell = 2
-        search_space_cls = IncrementSearchSpace()
-        search_space = search_space_cls.get_search_space()
-        submodel_manager = MicroCitationManager(args)
-        action_list = \
-            search_space_cls.generate_action_list(cell=args.num_of_cell)
-        if hasattr(args, "predict_hyper") and args.predict_hyper:
-                action_list = action_list + ["learning_rate", "dropout", "weight_decay", "hidden_unit"]
-        print('action list: ', action_list)
+    trainer = Evolution_Trainer(args)
+    if args.mode == 'train':
+        print(args)
+        trainer.train()
+    elif args.mode == 'derive':
+        trainer.derive()
     else:
-        print('ERROR: Unrecognized search mode: ', args.search_mode)
-        exit(-1)
-
-    print("Search space:")
-    print(search_space)
-    print("Generated Action List: ")
-    print(action_list)
-    # exit(0)
-    # deque with individuals (indexes to search space on action list)
-    population = deque()
-    # deque with the accuracies for each of the individuals in order
-    accs = deque()
-    cycles = args.cycles
-
-    population_size = args.population_size
-    sample_size = args.sample_size
-
-    # Initialize population with random architectures
-    print("\n\n===== Evaluating initial random population =====")
-    start_initial_population_time = time.time()
-    while len(population) < population_size:
-        # print('adding individual #:', len(population))
-        individual = generate_random_individual(search_space,
-                                                action_list)
-        _, ind_acc = \
-            submodel_manager.train(form_gnn_info(_construct_action([individual],
-                                                                   action_list,
-                                                                   search_space)[0], args),
-                                   format=args.format)
-        print(f"individual: {individual}, val_score:{ind_acc}")
-        accs.append(ind_acc)
-        population.append(individual)
-    end_initial_pop_time = time.time()
-    print("Time elapsed initializing population: " +
-          str(end_initial_pop_time - start_initial_population_time))
-    print("===== Evaluating initial random population DONE ====")
-
-    print("\n\n===== Evolution ====")
-    start_evolution_time = time.time()
-    while cycles > 0:
-        sample = []  # list with indexes to population individuals
-        sample_accs = []  # accuracies of the sampled individuals
-        while len(sample) < sample_size:
-            candidate = np.random.randint(0, len(population))
-            sample.append(population[candidate])
-            sample_accs.append(accs[candidate])
-
-        # Get best individual on sample to serve as parent
-        max_sample_acc_index, max_sample_acc = \
-            get_best_individual_accuracy(sample_accs)
-        parent = sample[max_sample_acc_index]
-        # print('parent: ', parent)
-        child = mutate_individual(parent, search_space, action_list)
-        # print('child: ', child)
-        _, child_acc = \
-            submodel_manager.train(form_gnn_info(_construct_action([child],
-                                                                   action_list,
-                                                                   search_space)[0], args),
-                                   format=args.format)
-        # print('child acc: ', child_acc)
-        print(f"parent: {parent}, val_score:{max_sample_acc} | child: {child}, val_score:{child_acc}")
-        accs.append(child_acc)
-        population.append(child)
-        if cycles % args.eval_cycle == 0:
-            derive_from_population(args.random_seed,
-                                   _construct_action(population,
-                                                     action_list,
-                                                     search_space),
-                                   accs,
-                                   submodel_manager,
-                                   args)
-        # Remove oldest individual (Aging/Regularized evolution)
-        population.popleft()
-        accs.popleft()
-        cycles -= 1
-    end_evolution_time = time.time()
-    print('Time spend on evolution: ' +
-          str(end_evolution_time - start_evolution_time))
-    print('Total elapsed time: ' +
-          str(end_evolution_time - start_initial_population_time))
-    print("===== Evolution DONE ====")
+        raise Exception("[!] Mode not found: ", args.mode)
 
 
 if __name__ == "__main__":
