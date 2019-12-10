22093bd28988a22c6a43e630ca484155df38a563
mhnnunes@pandora <mhnnunes@dcc.ufmg.br>
2019-12-10 15:21:42 -0300

    Update exp combinations script

--- generate_experiment_combinations.sh
+++ generate_experiment_combinations.sh
@@ -5,17 +5,10 @@ VERSION=$1
 if [[ $VERSION == "mod" ]]; then
   echo "======== Modified GraphNAS ========"
   for SEARCH_MODE in macro micro; do
+    echo "SEARCH_MODE: ${SEARCH_MODE}"
     for SEED in 123 42 10; do
       for DATASET in Citeseer Cora Pubmed; do
-        echo "nohup python -u -m graphnas.main \
-              --dataset ${DATASET} \
-              --population_size 100 \
-              --sample_size 25 \
-              --random_seed ${SEED} \
-              --search_mode ${SEARCH_MODE} \
-              > results_${SEARCH_MODE}_${DATASET}_${SEED}_evolution \
-              2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution
-              "
+        echo "nohup python -u -m graphnas.main --dataset ${DATASET} --optimizer EA --population_size 100 --sample_size 25 --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_evolution 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution &"
         # python -u -m graphnas.main \
         #           --dataset ${DATASET} \
         #           --population_size 100 \
@@ -26,15 +19,18 @@ if [[ $VERSION == "mod" ]]; then
         #           2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution
       done
     done  
+    echo ""
+    echo ""
   done  
 fi
 
 if [[ $VERSION == "orig" ]]; then
   echo "======== Original GraphNAS ========"
   for SEARCH_MODE in macro micro; do
+    echo "SEARCH_MODE: ${SEARCH_MODE}"
     for SEED in 123 42 10; do
       for DATASET in Citeseer Cora Pubmed; do
-        echo "nohup python -u -m graphnas.main --dataset ${DATASET} --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_RL 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL &"
+        echo "nohup python -u -m graphnas.main --dataset ${DATASET} --optimizer RL --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_RL 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL &"
         # python -u -m graphnas.main \
         #           --dataset ${DATASET} \
         #           --random_seed ${SEED} \
@@ -43,5 +39,7 @@ if [[ $VERSION == "orig" ]]; then
         #           2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL
       done
     done  
+    echo ""
+    echo ""
   done
 fi
