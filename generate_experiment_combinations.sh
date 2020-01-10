3358215cb2d045e451110fe54ef4f157beac6ab1
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-01-09 22:30:28 -0300

    Add supervised tag to extra datasets

--- generate_experiment_combinations.sh
+++ generate_experiment_combinations.sh
@@ -7,8 +7,19 @@ if [[ $VERSION == "mod" ]]; then
   for SEARCH_MODE in macro micro; do
     echo "SEARCH_MODE: ${SEARCH_MODE}"
     for SEED in 123 42 10; do
-      for DATASET in Citeseer Cora Pubmed CS Physics Computers Photo; do
+      for DATASET in Citeseer Cora Pubmed; do
         echo "nohup python -u -m graphnas.main --dataset ${DATASET} --optimizer EA --population_size 100 --sample_size 25 --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_evolution 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution &"
+        # python -u -m graphnas.main \
+        #           --dataset ${DATASET} \
+        #           --population_size 100 \
+        #           --sample_size 25 \
+        #           --random_seed ${SEED} \
+        #           --search_mode ${SEARCH_MODE} \
+        #           > results_${SEARCH_MODE}_${DATASET}_${SEED}_evolution \
+        #           2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution
+      done
+      for DATASET in CS Physics Computers Photo; do
+        echo "nohup python -u -m graphnas.main --dataset ${DATASET} --supervised True --optimizer EA --population_size 100 --sample_size 25 --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_evolution 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_evolution &"
         # python -u -m graphnas.main \
         #           --dataset ${DATASET} \
         #           --population_size 100 \
@@ -29,8 +40,17 @@ if [[ $VERSION == "orig" ]]; then
   for SEARCH_MODE in macro micro; do
     echo "SEARCH_MODE: ${SEARCH_MODE}"
     for SEED in 123 42 10; do
-      for DATASET in Citeseer Cora Pubmed CS Physics Computers Photo; do
+      for DATASET in Citeseer Cora Pubmed; do
         echo "nohup python -u -m graphnas.main --dataset ${DATASET} --optimizer RL --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_RL 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL &"
+        # python -u -m graphnas.main \
+        #           --dataset ${DATASET} \
+        #           --random_seed ${SEED} \
+        #           --search_mode ${SEARCH_MODE} \
+        #           > results_${SEARCH_MODE}_${DATASET}_${SEED}_RL \
+        #           2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL
+      done
+      for DATASET in CS Physics Computers Photo; do
+        echo "nohup python -u -m graphnas.main --dataset ${DATASET} --supervised True --optimizer RL --random_seed ${SEED} --search_mode ${SEARCH_MODE} > results_${SEARCH_MODE}_${DATASET}_${SEED}_RL 2> err_${SEARCH_MODE}_${DATASET}_${SEED}_RL &"
         # python -u -m graphnas.main \
         #           --dataset ${DATASET} \
         #           --random_seed ${SEED} \
