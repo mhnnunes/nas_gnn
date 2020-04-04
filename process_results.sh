99c2ede1d8f1dada087b0ca470e1bc472f7b0c52
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-04-04 14:17:54 -0300

    Rename DATABASE to DATASET and add RS

--- process_results.sh
+++ process_results.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-DATABASE=$1
+DATASET=$1
 DIR=$2
 TYPE=$3
 
 if [[ $TYPE == ""  ]]; then
-  cat ${DIR}/results_evolution_${DATABASE} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
-  cat ${DIR}/results_evolution_${DATABASE} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
-  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATABASE}
+  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
+  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
+  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATASET}
   rm ${DIR}/parent_temp ${DIR}/child_temp
-  cat ${DIR}/results_evolution_${DATABASE} | grep -E '(time|Time)' > ${DIR}/time_${DATABASE}
-  START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_evolution_${DATABASE} | cut -d':' -f 1`
-  END_INITIAL=`grep -n initial\ random\ population\ D  ${DIR}/results_evolution_${DATABASE} | cut -d':' -f 1`
+  cat ${DIR}/results_evolution_${DATASET} | grep -E '(time|Time)' > ${DIR}/time_${DATASET}
+  START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_evolution_${DATASET} | cut -d':' -f 1`
+  END_INITIAL=`grep -n initial\ random\ population\ D  ${DIR}/results_evolution_${DATASET} | cut -d':' -f 1`
   DIFF=`echo  $END_INITIAL - $START_INITIAL  + 1 | bc`
-  head -n ${END_INITIAL} ${DIR}/results_evolution_${DATABASE} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATABASE}
-  echo "Mean,Median,Best" > ${DIR}/population_stats_${DATABASE}
-  cat ${DIR}/results_evolution_${DATABASE} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATABASE}
+  head -n ${END_INITIAL} ${DIR}/results_evolution_${DATASET} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATASET}
+  echo "Mean,Median,Best" > ${DIR}/population_stats_${DATASET}
+  cat ${DIR}/results_evolution_${DATASET} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATASET}
 elif [[ $TYPE == "RS" ]]; then
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep ^val_score | cut -d',' -f1 | cut -d':' -f2 > ${DIR}/val_scores_${TYPE}_${DATABASE}
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep time | cut -d':' -f2 | sed -e 's/\ //g' > ${DIR}/time_${TYPE}_${DATABASE}
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep ^val_score | cut -d',' -f1 | cut -d':' -f2 > ${DIR}/val_scores_${TYPE}_${DATASET}
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep time | cut -d':' -f2 | sed -e 's/\ //g' > ${DIR}/time_${TYPE}_${DATASET}
+  cat ${DIR}/results_RS_${dataset} | grep BEST | grep Accuracy | cut -d' ' -f5 > ${mode}_results/seed_${seed}/best_acc_${dataset}
 else
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
-  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATABASE}_${TYPE}
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
+  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATASET}_${TYPE}
   rm ${DIR}/parent_temp ${DIR}/child_temp
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep -E '(time|Time)' > ${DIR}/time_${DATABASE}_${TYPE}
-  START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_${TYPE}_${DATABASE} | cut -d':' -f 1`
-  END_INITIAL=`grep -n initial\ random\ population\ D  ${DIR}/results_${TYPE}_${DATABASE} | cut -d':' -f 1`
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep -E '(time|Time)' > ${DIR}/time_${DATASET}_${TYPE}
+  START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_${TYPE}_${DATASET} | cut -d':' -f 1`
+  END_INITIAL=`grep -n initial\ random\ population\ D  ${DIR}/results_${TYPE}_${DATASET} | cut -d':' -f 1`
   DIFF=`echo  $END_INITIAL - $START_INITIAL  + 1 | bc`
-  head -n ${END_INITIAL} ${DIR}/results_${TYPE}_${DATABASE} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATABASE}_${TYPE}
-  echo "Mean,Median,Best" > ${DIR}/population_stats_${DATABASE}_${TYPE}
-  cat ${DIR}/results_${TYPE}_${DATABASE} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATABASE}_${TYPE}
+  head -n ${END_INITIAL} ${DIR}/results_${TYPE}_${DATASET} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATASET}_${TYPE}
+  echo "Mean,Median,Best" > ${DIR}/population_stats_${DATASET}_${TYPE}
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATASET}_${TYPE}
 fi
-
