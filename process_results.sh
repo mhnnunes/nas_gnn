a12580543fd6b76ea4178fe5d368acc3ab9a52df
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-04-12 11:12:28 -0300

    Processing best accuracy results for RL and RS

--- process_results.sh
+++ process_results.sh
@@ -16,10 +16,12 @@ if [[ $TYPE == ""  ]]; then
   head -n ${END_INITIAL} ${DIR}/results_evolution_${DATASET} | tail -n ${DIFF} | grep val_score | cut -d':' -f3 > ${DIR}/initial_population_${DATASET}
   echo "Mean,Median,Best" > ${DIR}/population_stats_${DATASET}
   cat ${DIR}/results_evolution_${DATASET} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATASET}
+elif [[ $TYPE == "RL" ]]; then
+  cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f3 | sed -e "s/^\ //g" | sed -e "s/\ +\/-\ /,/g" > ${DIR}/best_acc_${TYPE}_${DATASET}
 elif [[ $TYPE == "RS" ]]; then
   cat ${DIR}/results_${TYPE}_${DATASET} | grep ^val_score | cut -d',' -f1 | cut -d':' -f2 > ${DIR}/val_scores_${TYPE}_${DATASET}
   cat ${DIR}/results_${TYPE}_${DATASET} | grep time | cut -d':' -f2 | sed -e 's/\ //g' > ${DIR}/time_${TYPE}_${DATASET}
-  cat ${DIR}/results_RS_${dataset} | grep BEST | grep Accuracy | cut -d' ' -f5 > ${mode}_results/seed_${seed}/best_acc_${dataset}
+  cat ${DIR}/results_RS_${dataset} | grep BEST | grep Accuracy | cut -d' ' -f5 > ${DIR}/best_acc_${DATASET}
 else
   cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
   cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
