d41cc85cecca1f7b46ba848e07a9bca2f70ccd95
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-07-26 16:14:01 -0300

    Add cache dir param to virtualenv script

--- virtualenv_script.sh
+++ virtualenv_script.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# This script creates a virtual environment using Miniconda and 
+# installs the dependencies necessary to run the code in this 
+# repository. This script considers that the computer is using
+# CUDA 10.1. If this is not your CUDA version, change
+# the lines with comments highlighted below
+
+CACHE=""
+if [[ $1 != "" ]]; then
+	CACHE="--cache-dir ${1}"
+fi
+
+echo "CACHE: ${CACHE}"
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
@@ -6,18 +21,20 @@ conda create -y --name py37 python=3.7.4 && \
 conda clean -ya
 conda activate py37
 conda install pip
+# IMPORTANT: if your computer is not using CUDA 10.1 the line below 
+# has to contain a different version of magma-cuda.
 conda install -y -c pytorch magma-cuda101 pytorch torchvision && conda clean -ya
 conda install h5py
-pip install --cache-dir /srv/mhnnunes/pipcache h5py-cache && \
-pip install --cache-dir /srv/mhnnunes/pipcache torchnet
+pip install ${CACHE} h5py-cache && \
+pip install ${CACHE} torchnet
 conda install requests && \
 conda install graphviz && conda clean -ya
 conda install -y -c menpo opencv3 && conda clean -ya
 
 
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache torch-scatter  && \
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache torch-sparse && \
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache torch-cluster && \
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache torch-spline-conv && \
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache torch-geometric && \
-pip install --verbose --cache-dir /srv/mhnnunes/pipcache dgl
+pip install --verbose ${CACHE} torch-scatter  && \
+pip install --verbose ${CACHE} torch-sparse && \
+pip install --verbose ${CACHE} torch-cluster && \
+pip install --verbose ${CACHE} torch-spline-conv && \
+pip install --verbose ${CACHE} torch-geometric && \
+pip install --verbose ${CACHE} dgl
