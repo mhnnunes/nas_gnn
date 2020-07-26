7cbdac5b55a930110d2d2d47fdc725507ab639cd
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-07-26 16:21:18 -0300

    Update script for processing text results

--- process_results.sh
+++ process_results.sh
@@ -5,9 +5,9 @@ DIR=$2
 TYPE=$3
 
 if [[ $TYPE == ""  ]]; then
-  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
-  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
-  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATASET}
+  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f2,3 | sed -e "s/^\ *//g" | sed -e "s/\ *val_score:\ */>/g"  > ${DIR}/parent_temp
+  cat ${DIR}/results_evolution_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f2,3 | sed -e "s/^\ *//g" | sed -e "s/\ ,\ *val_score:\ */>/g" > ${DIR}/child_temp
+  paste ${DIR}/parent_temp ${DIR}/child_temp -d'>' > ${DIR}/parent_child_${DATASET}
   rm ${DIR}/parent_temp ${DIR}/child_temp
   cat ${DIR}/results_evolution_${DATASET} | grep -E '(time|Time)' > ${DIR}/time_${DATASET}
   START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_evolution_${DATASET} | cut -d':' -f 1`
@@ -18,18 +18,23 @@ if [[ $TYPE == ""  ]]; then
   cat ${DIR}/results_evolution_${DATASET} | grep STATS | awk '{print $4,$5,$6}' | sed -e 's/\ /,/g' >> ${DIR}/population_stats_${DATASET}
 elif [[ $TYPE == "RL" ]]; then
   if [[ `echo ${DIR} | grep micro | wc -l` -gt 0 ]]; then
-    cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f5 | sed -e "s/^\ //g" | sed -e "s/\ +\/-\ /,/g" > ${DIR}/best_acc_${TYPE}_${DATASET}
+    ACTIONS=`cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f 3,4 | sed -e "s/\],\ 'hyper_param':\ \[/,\ /g" | sed -e "s/^\ //g" | sed -e "s/\}//g"`
+    ACC=`cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f5 | sed -e "s/^\ //g" | sed -e "s/\ +\/-\ />/g"`
   else
-    cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f3 | sed -e "s/^\ //g" | sed -e "s/\ +\/-\ /,/g" > ${DIR}/best_acc_${TYPE}_${DATASET}
+    ACTIONS=`cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f2`
+    ACC=`cat ${DIR}/results_${TYPE}_${DATASET}  | grep "best results" | cut -d':' -f3 | sed -e "s/^\ //g" | sed -e "s/\ +\/-\ />/g"`
   fi
+  echo "${ACTIONS}>${ACC}" > ${DIR}/best_acc_${TYPE}_${DATASET}
 elif [[ $TYPE == "RS" ]]; then
   cat ${DIR}/results_${TYPE}_${DATASET} | grep ^val_score | cut -d',' -f1 | cut -d':' -f2 > ${DIR}/val_scores_${TYPE}_${DATASET}
   cat ${DIR}/results_${TYPE}_${DATASET} | grep time | cut -d':' -f2 | sed -e 's/\ //g' > ${DIR}/time_${TYPE}_${DATASET}
-  cat ${DIR}/results_RS_${dataset} | grep BEST | grep Accuracy | cut -d' ' -f5 > ${DIR}/best_acc_${DATASET}
+  ACC=`cat ${DIR}/results_${TYPE}_${DATASET} | grep BEST | grep Accuracy | cut -d':' -f2 | sed -e "s/^\ *//g"`
+  ACTIONS=`cat ${DIR}/results_${TYPE}_${DATASET} | grep BEST | grep Actions | cut -d':' -f2 | sed -e "s/^\ *//g" | sed -e "s/\[\[/\[/g" | sed -e "s/\]\]/\]/g"`
+  echo "${ACTIONS}>${ACC}" > ${DIR}/best_acc_${TYPE}_${DATASET}
 else
-  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f3 | sed 's/\ //g'  > ${DIR}/parent_temp
-  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f3 > ${DIR}/child_temp
-  paste ${DIR}/parent_temp ${DIR}/child_temp -d',' > ${DIR}/parent_child_${DATASET}_${TYPE}
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f1 | cut -d':' -f2,3 | sed -e "s/^\ *//g" | sed -e "s/\ *val_score:\ */>/g"  > ${DIR}/parent_temp
+  cat ${DIR}/results_${TYPE}_${DATASET} | grep parent | cut -d '|' -f2 | cut -d':' -f2,3 | sed -e "s/^\ *//g" | sed -e "s/\ ,\ *val_score:\ */>/g" > ${DIR}/child_temp
+  paste ${DIR}/parent_temp ${DIR}/child_temp -d'>' > ${DIR}/parent_child_${DATASET}_${TYPE} && sed -i "s/\ >/>/g"  ${DIR}/parent_child_${DATASET}_${TYPE}
   rm ${DIR}/parent_temp ${DIR}/child_temp
   cat ${DIR}/results_${TYPE}_${DATASET} | grep -E '(time|Time)' > ${DIR}/time_${DATASET}_${TYPE}
   START_INITIAL=`grep -n initial\ random\ population\ =  ${DIR}/results_${TYPE}_${DATASET} | cut -d':' -f 1`
