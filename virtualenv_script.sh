f3acffc6c30baf18b9dc83a170797c9f6107ddb0
Matheus Nunes @ZenBook <mhnnunes@dcc.ufmg.br>
2020-08-12 11:14:14 -0300

    Add versions of packages in virtualenv script

--- virtualenv_script.sh
+++ virtualenv_script.sh
@@ -11,7 +11,7 @@ if [[ $1 != "" ]]; then
 	CACHE="--cache-dir ${1}"
 fi
 
-echo "CACHE: ${CACHE}"
+echo "USING CACHE: ${CACHE}"
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
@@ -23,18 +23,19 @@ conda activate py37
 conda install pip
 # IMPORTANT: if your computer is not using CUDA 10.1 the line below 
 # has to contain a different version of magma-cuda.
-conda install -y -c pytorch magma-cuda101 pytorch torchvision && conda clean -ya
-conda install h5py
-pip install ${CACHE} h5py-cache && \
-pip install ${CACHE} torchnet
-conda install requests && \
-conda install graphviz && conda clean -ya
+conda install -y -c pytorch=1.3.1 magma-cuda101=2.5.1 torchvision=0.4.2 && \
+conda clean -ya
+conda install h5py=2.9.0
+pip install --verbose ${CACHE} h5py-cache && \
+pip install --verbose ${CACHE} torchnet
+conda install requests=2.22.0 && \
+conda install graphviz=2.40.1 && conda clean -ya
 conda install -y -c menpo opencv3 && conda clean -ya
 
 
-pip install --verbose ${CACHE} torch-scatter  && \
-pip install --verbose ${CACHE} torch-sparse && \
-pip install --verbose ${CACHE} torch-cluster && \
-pip install --verbose ${CACHE} torch-spline-conv && \
-pip install --verbose ${CACHE} torch-geometric && \
-pip install --verbose ${CACHE} dgl
+pip install --verbose ${CACHE} torch-scatter==1.4.0  && \
+pip install --verbose ${CACHE} torch-sparse==0.4.3 && \
+pip install --verbose ${CACHE} torch-cluster==1.4.5 && \
+pip install --verbose ${CACHE} torch-spline-conv==1.1.1 && \
+pip install --verbose ${CACHE} torch-geometric==1.3.2 && \
+pip install --verbose ${CACHE} dgl==0.4.1
